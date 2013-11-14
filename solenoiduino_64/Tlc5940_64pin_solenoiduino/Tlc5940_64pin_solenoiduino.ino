ef637cb9608e9a27372edc47026d71a85701dad0
epicjefferson <epicjefferson@gmail.com>
2013-11-14 14:27:00 -0400

    adding solenoiduino firmware

---
+++ solenoiduino_64/Tlc5940_64pin_solenoiduino/Tlc5940_64pin_solenoiduino.ino
@@ -0,0 +1,75 @@
+#include "Tlc5940.h"
+
+const long baudRate = 57600; 
+boolean isset = false;
+int incomingByte;
+int firstByte;
+int secondByte;
+int solenoidPin;
+int velocity;
+//long offTimes[8];
+//long now;
+
+void  setup()
+{
+  // start serial communication
+  //initialize the TLC5940
+  Serial.begin(baudRate);
+  Tlc.init();
+}
+
+
+void loop()
+{
+  // check serial in for incoming messages
+  // a complete messages consists of two bytes
+  // the bit mask looks as followed:
+  //
+  // 0 P P P P P P V   1 V V V V V V V
+  //
+  // 0, 1 :  header of first and second byte
+  // P    :  Pin Number 1-64 (currently only 44 are in use)
+  // V    :  velocity (0-4095 in 256 steps)
+  if(Serial.available() != 0)
+  {
+    incomingByte = Serial.read();
+    
+    
+    // CODE FOR HANDSHAKE
+    if(incomingByte == 255)
+    {
+      Serial.print("SOL 0 1");
+    }
+    // END CODE FOR HANDSHAKE
+    
+    if((incomingByte >> 7) == 0)
+    {
+      firstByte = incomingByte;
+    }
+    else
+    {
+      secondByte = incomingByte;
+      // as second bytes is received, the message is complete
+      isset = true;
+    }
+  }
+  
+  // if a full message is received:
+  //   - apply pwm signal (velocity) to respective pin
+  
+  
+  if(isset)
+  {
+    //here's the bit twiddling stuff that needs to be altered
+    //to address up to 44 pins, not just 16
+    solenoidPin = firstByte >> 1;
+    velocity = ((firstByte % 1) << 7) + (secondByte % 128);
+    
+    //since the max 'time on' was 1023, map to 4095 (which is the 
+    //highest value for each pin on the TLC5940)
+    velocity = map(velocity,0,255,0,4095);
+    Tlc.set(solenoidPin, velocity);
+    Tlc.update();
+    isset = false;
+  }
+}
---
+++ solenoiduino_64/readme.txt
@@ -0,0 +1,17 @@
+Este archivo de Solenoiduino incluye:
+
+Abstraction para Pure Data + archivo help
+Código para Arduino UNO
+
+Originalmente creado por Roman Haefeli (reduzent[at]gmail[dot]com)
+y modificado para el Meta-Piano Extendido por Epic Jefferson.
+
+__________________________________________________________________________
+
+This Solenoiduino file includes:
+
+Pure Data Abstraction + help file
+Arduino UNO Firmware
+
+Originally created by Roman Haefeli (reduzent[at]gmail[dot]com)
+and modified for the Meta-Piano Extended by Epic Jefferson.
